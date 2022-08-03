bee488f97f7de2934de52a8c23ce609233e15a4c
Dominik Sigmund <dominik.sigmund@br.de>
2022-08-03 17:46:59 +0200

    removed statusbar

--- app/src/main/java/eu/webdad/dasbibelquiz/MainActivity.kt
+++ app/src/main/java/eu/webdad/dasbibelquiz/MainActivity.kt
@@ -1,9 +1,12 @@
 package eu.webdad.dasbibelquiz
 
+import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.view.WindowManager
+import android.webkit.ConsoleMessage
+import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
@@ -13,34 +16,32 @@ import androidx.core.view.WindowInsetsControllerCompat
 
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (Build.VERSION.SDK_INT < 16) {
-            window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        if (Build.VERSION.SDK_INT < 30) {
+            val decorView: View = window.decorView
+            val uiOptions: Int = View.SYSTEM_UI_FLAG_FULLSCREEN
+            decorView.setSystemUiVisibility(uiOptions)
         } else {
-            // Hide the status bar.
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-            // Remember that you should never show the action bar if the
-            // status bar is hidden, so hide that too if necessary.
-            actionBar?.hide()
+            window.decorView.windowInsetsController?.hide(
+                android.view.WindowInsets.Type.systemBars()
+            );
         }
-
-        val windowInsetsController =
-            ViewCompat.getWindowInsetsController(window.decorView) ?: return
-        // Configure the behavior of the hidden system bars
-        windowInsetsController.systemBarsBehavior =
-            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-        // Hide both the status bar and the navigation bar
-        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
 
         setContentView(R.layout.activity_main)
 
 
 
         val myWebView: WebView = findViewById(R.id.webview)
+        myWebView.webChromeClient = object : WebChromeClient() {
+            override fun onConsoleMessage(message: ConsoleMessage): Boolean {
+                Log.d("MyApplication", "${message.message()} -- From line " +
+                        "${message.lineNumber()}")
+                return true
+            }
+        }
         val settings: WebSettings = myWebView.settings
         settings.domStorageEnabled = true
         settings.javaScriptEnabled = true
