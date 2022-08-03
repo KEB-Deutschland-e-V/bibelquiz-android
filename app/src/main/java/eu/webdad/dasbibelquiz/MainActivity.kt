23e2697347dd43592cf4b07288cd39b401a71217
Dominik Sigmund <dominik.sigmund@br.de>
2022-08-03 16:23:41 +0200

    Fixed Webview, ready for testing

--- app/src/main/java/eu/webdad/dasbibelquiz/MainActivity.kt
+++ app/src/main/java/eu/webdad/dasbibelquiz/MainActivity.kt
@@ -1,16 +1,52 @@
 package eu.webdad.dasbibelquiz
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.webkit.WebSettings
 import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (Build.VERSION.SDK_INT < 16) {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        } else {
+            // Hide the status bar.
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+            // Remember that you should never show the action bar if the
+            // status bar is hidden, so hide that too if necessary.
+            actionBar?.hide()
+        }
+
+        val windowInsetsController =
+            ViewCompat.getWindowInsetsController(window.decorView) ?: return
+        // Configure the behavior of the hidden system bars
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        // Hide both the status bar and the navigation bar
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+
         setContentView(R.layout.activity_main)
+
+
+
         val myWebView: WebView = findViewById(R.id.webview)
-        myWebView.settings.javaScriptEnabled = true
-        setContentView(myWebView)
+        val settings: WebSettings = myWebView.settings
+        settings.domStorageEnabled = true
+        settings.javaScriptEnabled = true
+        settings.cacheMode = WebSettings.LOAD_DEFAULT
+
+        //setContentView(myWebView)
         myWebView.loadUrl("https://dasbibelquiz.de")
     }
 }
